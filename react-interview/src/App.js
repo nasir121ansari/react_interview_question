d80c9f5aade534b22d4176b07f508c4731d05084
Md Nasir Ansari <nasir121ansari@gmail.com>
2024-05-30 17:22:38 +0530

    basic setup

---
+++ react-interview/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+
+function App() {
+  return (
+    <div className="App">
+     <h1 className='bg-dark text-white'>hello</h1>
+    </div>
+  );
+}
+
+export default App;
