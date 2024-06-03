c329fe9c6ae4854e35c7e997a4462448914b7571
Md Nasir Ansari <nasir121ansari@gmail.com>
2024-06-03 11:56:16 +0530

    counter

--- react-interview/src/App.js
+++ react-interview/src/App.js
@@ -2,7 +2,8 @@ import React from "react";
 import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Fetch_data_from_url from "./Components/Fetch_data_from_url";
+import Fetch_data_from_url from "./Components/fethData/Fetch_data_from_url";
+import { Counter_incr_decr } from "./Components/counter/Counter_incr_decr";
 
 function Home() {
   return (
@@ -11,6 +12,9 @@ function Home() {
       <ol>
         <li>
           <Link to="/fetch-data">Fetch Data from Url and display that</Link>
+        </li>
+        <li>
+          <Link to="/counter">Counter Increment and Decrement</Link>
         </li>
       </ol>
     </div>
@@ -24,6 +28,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/fetch-data" element={<Fetch_data_from_url />} />
+          <Route path="/counter" element={<Counter_incr_decr />} />
         </Routes>
       </div>
     </Router>
